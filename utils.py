368c7d3236e3d054e19848f3e693b23d24c9d8e3
nishukothari <nishuk@umich.edu>
2021-12-15 13:20:49 -0800

    2 dimensional fix

--- utils.py
+++ utils.py
@@ -301,7 +301,7 @@ def runNetwork(train, num_epochs, net, dataset, batch=32, file_extension=""):
         loader = get_dataloader(dataset, batch)
         for embeddings, label in tqdm(loader):        
             prediction = net.forward(embeddings)
-            loss = criterion(prediction, np.argmax(label, axis=1))
+            loss = criterion(prediction, label)
             prediction = prediction.detach().numpy()
             #prediction = np.apply_along_axis(predict_one_hot, axis=1, arr=prediction)
             prediction = np.rint(prediction)
